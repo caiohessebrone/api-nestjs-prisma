8697f182f389c62414352a21bd0d3229d9a88b2b
caiohessebrone <caiobrone77@gmail.com>
2022-03-15 12:29:50 -0300

    Criada a rota de user, iniciada o prisma para conexao com Postgres

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,13 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
